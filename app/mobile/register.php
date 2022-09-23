895bb2bbbea4fb04e25a668abf40994d515be8aa
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-09-23 10:57:12 +0800

    Update register.php

--- app/mobile/register.php
+++ app/mobile/register.php
@@ -10,10 +10,9 @@ require_once 'core/config.php';
 $data = json_decode(file_get_contents("php://input"));
 
 if(isset($data->username) && !empty($data->username) ){
-	$user_fname = $mysqli_connect->real_escape_string($data->user_fname);
-	$user_mname = "";//$mysqli_connect->real_escape_string($_POST['user_mname']);
-    $user_lname = $mysqli_connect->real_escape_string($data->user_lname);
-    $category = "U";//$mysqli_connect->real_escape_string($_POST['category']);
+	$user_fullname = $mysqli_connect->real_escape_string($data->user_fullname);
+    $user_address = $mysqli_connect->real_escape_string($data->user_address);
+    $category = "R";
 	
 	$username = $mysqli_connect->real_escape_string($data->username);
 	$password = $mysqli_connect->real_escape_string($data->password);
@@ -26,7 +25,7 @@ if(isset($data->username) && !empty($data->username) ){
 	if($count_rows[0] > 0){
 		echo -1;
 	}else{
-		$sql= $mysqli_connect->query("INSERT INTO `tbl_users`(`user_fname`, `user_mname`, `user_lname`, `username`, `password`, `category`, `date_updated`) VALUES ('$user_fname','$user_mname','$user_lname','$username',md5('$password'),'$category','$date')");
+		$sql= $mysqli_connect->query("INSERT INTO `tbl_users`(`user_fullname`, `user_address`, `username`, `password`, `user_category`, `date_updated`) VALUES ('$user_fullname','$user_address','$username',md5('$password'),'$category','$date')");
 			
 		if($sql){
 			$user_id = $mysqli_connect->insert_id;
