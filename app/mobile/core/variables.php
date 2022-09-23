b6e7b7b087c7839646af1ac1baf56f5df99d001f
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-09-23 08:37:10 +0800

    Update variables.php

--- app/mobile/core/variables.php
+++ app/mobile/core/variables.php
@@ -1,13 +1,10 @@
 <?php
 /** Core Path **/
-define("view","pages/");
-
-
 define("host","localhost");
 define("username","u981310152_mefa_root");
 define("password","Mefa211!");
 define("database","u981310152_mefa_db");
 
 //inside dir
-define ("VALUE",serialize (array ("","my_functions.php")));
+define ("VALUE",serialize (array("","my_functions.php")));
 ?>
