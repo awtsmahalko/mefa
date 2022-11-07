cb572c2c5b22cbaaa4f32fd877cedc8c93270962
awtsmahalko <56856915+awtsmahalko@users.noreply.github.com>
2022-11-07 12:38:31 +0800

    Update notification.php

--- app/api/notification.php
+++ app/api/notification.php
@@ -4,7 +4,7 @@ include '../core/config.php';
 $form = array(
     'notif_title'   => 'Fire Alert',
     'notif_message' => 'There is a fire in your area',
-    'coordinates'   => '10.63629241849944,122.9585436492834', //'10.6826927,122.9438081',
+    'coordinates'   => '10.63629241849944,122.9585436492834', //'10.6826927,122.9438081', FROM DEVICE
 );
 
 $Notif = new Notifications();
