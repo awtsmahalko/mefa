0d85d15d422c5ae0f88c8492ad8a1d2c65e432b1
jepjepj <58154782+jepjepj@users.noreply.github.com>
2023-02-03 14:38:57 +0800

    Update getPropertyDetails.php

--- app/mobile/getPropertyDetails.php
+++ app/mobile/getPropertyDetails.php
@@ -11,23 +11,40 @@ $data = json_decode(file_get_contents("php://input"));
 
 if(isset($data->property_id) && !empty($data->property_id )){
 
-	$property_id = $mysqli_connect->real_escape_string($data->property_id);
+	$property_id = $mysqli_connect->real_escape_string($data->property_id) * 1;
+    $user_id = $mysqli_connect->real_escape_string($data->user_id);
 
 	$response = array();
-    
-	$fetch = $mysqli_connect->query("SELECT * FROM tbl_properties where property_id='$property_id' ") or die(mysql_error());
-	$row = $fetch->fetch_assoc();
 
-    $list = array();
-    $list['property_id'] = $row['property_id'];
-    $list['property_name'] = $row['property_name'];
-    $list['property_address'] = $row['property_address'];
-    $list['property_radius'] = $row['property_radius'];
+    if($property_id == -1){
+        $fetch = $mysqli_connect->query("SELECT user_resident_coordinates, user_radius FROM tbl_users where user_id='$user_id' ") or die(mysql_error());
+	    $row = $fetch->fetch_assoc();
 
-    $coordinates = explode(",",$row['property_coordinates']);
+        $list = array();
+        $list['property_id'] = -1;
+        $list['property_name'] = "Home";
+        $list['property_address'] = "Home";
+        $list['property_radius'] = $row['user_radius'];
 
-    $list['property_lat'] = $coordinates[0];
-    $list['property_lng'] = $coordinates[1];
+        $coordinates = explode(",",$row['user_resident_coordinates']);
+
+        $list['property_lat'] = $coordinates[0];
+        $list['property_lng'] = $coordinates[1];
+
+    }else{
+        $fetch = $mysqli_connect->query("SELECT * FROM tbl_properties where property_id='$property_id' ") or die(mysql_error());
+	    $row = $fetch->fetch_assoc();
+        $list = array();
+        $list['property_id'] = $row['property_id'];
+        $list['property_name'] = $row['property_name'];
+        $list['property_address'] = $row['property_address'];
+        $list['property_radius'] = $row['property_radius'];
+
+        $coordinates = explode(",",$row['property_coordinates']);
+
+        $list['property_lat'] = $coordinates[0];
+        $list['property_lng'] = $coordinates[1];
+    }
 
 	echo json_encode($list);
 
